63b7656c755bd1e0a4da91d524f453f9d03d0754
Diego Bryan <thediegobryan@gmail.com>
2022-06-22 13:13:12 -0400

    add difficulty setting

--- mystery_word.py
+++ mystery_word.py
@@ -4,6 +4,29 @@ solution = []
 user_guess = []
 tries_left = 8
 
+def choose_word(words):
+    difficulty = input("Choose a Difficulty.\neasy (4-6 letters) | normal (6-8 letters) | hard (8+ letters) \n")
+    #verifies user difficulty is valid
+    while difficulty != 'easy' and difficulty!= 'normal' and difficulty != 'hard':
+        print('Not an option, pick again')
+        difficulty = input("Choose a Difficulty.\neasy (4-6 letters) | normal (6-8 letters) | hard (8+ letters) \n")
+    
+    list_of_words = words.split()
+    if difficulty == 'easy':
+        for word in list_of_words.copy():
+            if len(word) > 6:
+                list_of_words.remove(word)
+    elif difficulty == 'normal':
+        for word in list_of_words.copy():
+            if len(word) > 8 or len(word) < 6:
+                list_of_words.remove(word)
+    elif difficulty == 'hard':
+        for word in list_of_words.copy():
+            if len(word) < 8:
+                list_of_words.remove(word)
+    
+    return random.choice(list_of_words)
+
 def prepare_game(word):
     word_list = list(word)
     for char in word_list:
@@ -12,7 +35,7 @@ def prepare_game(word):
     
 def start_game(tries):
     while tries > 0 and '_' in user_guess:
-        print(user_guess)
+        print(" ".join(user_guess))
         letter_guess = input("Guess a letter? ").lower()
         if letter_guess in solution:
             #these loops allow for multiples of the same letter
@@ -22,19 +45,18 @@ def start_game(tries):
         else:
             tries -= 1
             print(f"You have {tries} remaining")
-    
+    #when loop ends determines if you won or lost
     if '_' in user_guess:
         print("You Lose! Try Again")
     else:
         print("You Won!")
 
+
 def play_game(file):
     with open(file) as open_file:
         read_file = open_file.read()
-    
-    prepare_game(random.choice(read_file.split()))
-    print(solution)
-
+    chosen_word = choose_word(read_file)
+    prepare_game(chosen_word)
     start_game(tries_left)
 
 if __name__ == "__main__":
